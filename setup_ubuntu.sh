55f17c1a0d3d739b51a7d6f556be13e52d4ca5d8
Vu Le <vule20.cs@gmail.com>
2025-03-05 15:59:00 -0500

    conda installation

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -7,22 +7,22 @@ cp profile ~/.profile
 cp emacs ~/.emacs
 
 
-# # install anaconda
-# ANACONDA_VERSION="2024.06-1"
-# INSTALLER="Anaconda3-${ANACONDA_VERSION}-Linux-x86_64.sh"
-# wget https://repo.anaconda.com/archive/$INSTALLER -O /tmp/$INSTALLER
+# install anaconda
+ANACONDA_VERSION="2024.06-1"
+INSTALLER="Anaconda3-${ANACONDA_VERSION}-Linux-x86_64.sh"
+wget https://repo.anaconda.com/archive/$INSTALLER -O /tmp/$INSTALLER
 
-# # Run the installer
-# sha256sum  /tmp/$INSTALLER
-# echo "Running Anaconda installer..."
-# rm -rf ~/anaconda 2>/dev/null
-# bash /tmp/$INSTALLER -b -p ~/anaconda
+# Run the installer
+sha256sum  /tmp/$INSTALLER
+echo "Running Anaconda installer..."
+rm -rf ~/anaconda 2>/dev/null
+bash /tmp/$INSTALLER -b -p ~/anaconda
 
-# # Initialize Conda
-# echo "Initializing Conda..."
-# eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
-# conda init
+# Initialize Conda
+echo "Initializing Conda..."
+eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
+conda init
 
-# # Clean up the installer
-# rm /tmp/$INSTALLER
-# echo "Anaconda installation is complete. Please restart your terminal or run 'source ~/.bashrc'."
+# Clean up the installer
+rm /tmp/$INSTALLER
+echo "Anaconda installation is complete. Please restart your terminal or run 'source ~/.bashrc'."
